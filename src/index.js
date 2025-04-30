7b382c1ce1e8a5f08242d52a39aadcbcd6c54da7
siv921 <asivets@me.com>
2025-04-30 10:51:47 +0100

    added initial javascript

---
+++ src/index.js
@@ -0,0 +1,51 @@
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = days[date.getDay()];
+
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${currentDay} ${hours}:${minutes} `;
+}
+
+let currentDate = new Date();
+let updateDay = document.querySelector("#day-time");
+
+updateDay.innerHTML = formatDate(currentDate);
+
+let city = document.querySelector("#update-city");
+city.addEventListener("submit", newCity);
+
+function newCity(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-city");
+  let city = searchInputElement.value;
+
+  let apiKey = `d102f34aao5ce8t34e64ea7aab7a37d2`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(updateTemperature);
+}
+
+console.log(response.data);
+function updateTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let currentTemp = document.querySelector("#main-degrees-value");
+  let newHeading = document.querySelector("h1");
+  newHeading.innerHTML = response.data.city;
+  currentTemp.innerHTML = temperature;
+}
